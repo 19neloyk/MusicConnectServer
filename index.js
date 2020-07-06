715ea2a6ba3beec124579d35fce25b109007f8f2
Neloy Kundu <chandrarani2001@gmail.com>
2020-07-05 22:07:43 -0400

    Added Spotify authentication

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+//Used for routing and handling requests
 const express = require('express');
+const app = express()
+
+//Used for database
 const mongoose = require('mongoose');
 
-const app = express()
+//Used for managing Spotify token
+const btoa = require("btoa"); // base64 encoder
+const axios = require("axios"); // http library
+const qs = require("qs"); // querystring parsing
+SPOTIFY_CLIENT_ID = "044b2c45e77f45aca8da89e338849b6a"
+SPOTIFY_CLIENT_SECRET = "c7d838bc6f3d40889cb11517afc8e3f3"
+SPOTIFY_CLIENT_CALLBACK_URL = "spotify-login-sdk-test-app://spotify-login-callback"
+ENCRYPTION_SECRET = "cFJLyifeUJUBFWdHzVbykfDmPHtLKLGzViHW9aHGmyTLD8hGXC"
 
 const User = require('./models/User')
 const Party = require('./models/Party')
@@ -26,7 +37,13 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true })
 
 app.listen(PORT,console.log(`Server started on port ${PORT}`))        
 
-
+// accept form-urlencoded submissions
+axios.interceptors.request.use(request => {
+    if (request.data && request.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+      request.data = qs.stringify(request.data);
+    }
+    return request;
+  });
 
 app.post('/newuser', (req,res) => {
     console.log('What is poppin')
@@ -75,15 +92,62 @@ app.post('/login', (req,res) => {
     });
 });
 
-app.get('/newparty', (req,res) => {
-    
-    res.json({"statement":"HELLOOOO"})
-    console.log('What is poopin')
-});
-
 app.post('/newparty', (req,res) => {
     res.send('hello');
     //Create new party
 });
 
+// POST /api/token
+app.post("/spotifyauth/token", ({body: {code: authorization_code}}, response) => {
+    console.log("REQUEST RECEIVED")
+    axios({
+      method: "POST",
+      url: "https://accounts.spotify.com/api/token",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Basic " + btoa(SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET)
+      },
+      data: {
+        grant_type: "authorization_code",
+        redirect_uri: SPOTIFY_CLIENT_CALLBACK_URL,
+        code: authorization_code
+      }
+    }).then(({data: json}) => {
+      // ▾▾▾ --- encrypted code --- ▾▾▾
+      // let encrypted_access_token = encrypt(json.access_token, ENCRYPTION_SECRET);
+      // response.set("Content-Type", "text/json").status(200).send(Object.assign({}, json, {refresh_token: encrypted_access_token}));
+      
+      // ▾▾▾ --- unencrypted code --- ▾▾▾
+      console.log("UNENCRYPTED CODE SENT")
+      response.set("Content-Type", "text/json").status(200).send(json);
+    }).catch(({response: err}) => {
+      console.log("ERROR SENT")
+      response.set("Content-Type", "text/json").status(402).send(err.data);
+    });
+  });
+  
+  // POST /api/refresh_token
+  app.post("/spotifyrefresh_token", ({body: {refresh_token}}, response) => {
+    console.log("REQUEST RECEIVED")
+    axios({
+      method: "POST",
+      url: "https://accounts.spotify.com/api/token",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Basic " + btoa(SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET)
+      },
+      data: {
+        grant_type: "refresh_token",
+        // ▾▾▾ --- unencrypted code --- ▾▾▾
+        refresh_token: refresh_token,
+        
+        // ▾▾▾ --- encrypted code --- ▾▾▾
+        // refresh_token: decrypt(refresh_token, ENCRYPTION_SECRET),
+      }
+    }).then(({data: json}) => {
+      response.set("Content-Type", "text/json").status(200).send(json);
+    }).catch(({response: err}) => {
+      response.set("Content-Type", "text/json").status(402).send(err.data);
+    });
+  });
 //app.post('/')
