e76bdd3163402794259ced10fb1857f30b37cc18
Neloy <neloy@cpe-98-14-55-146.nyc.res.rr.com>
2020-07-21 19:40:23 -0400

    Added password encryption

--- routes/accounts.js
+++ routes/accounts.js
@@ -3,8 +3,10 @@ const router = express.Router()
 //Used for database
 const mongoose = require('mongoose');
 const User = require('../models/User')
+//Used for password encryption
+const bcrypt = require('bcrypt')
 
-router.post('/newuser', (req,res) => {
+router.post('/newuser', async (req,res) => {
     console.log('What is poppin')
     const {username, email, password} = req.body
     User.findOne ({email:email})
@@ -19,17 +21,23 @@ router.post('/newuser', (req,res) => {
                 if (anotherUser) {
                     res.json({"heading":"Username taken",
                       "statement":"Use a different Username"})
-                    return
                 } else {
-                    //Create new user
-                    const newUser = new User ({
-                        name: username,
-                        email:email,
-                        password:password
-                    });
-                    newUser.save()
-                    res.json({"heading":"Success",
-                                    "statement":"Account created"});
+                    //Encrypt password for database
+                    bcrypt.hash(password,10,(err,encrypted) => { //encrypted represents the hashed password of the callback
+                        if (err){
+                            res.json({"heading":"Error",
+                                    "statement":"Could not create account. Please try again."});
+                        } else {    //Create new user
+                            const newUser = new User ({
+                                name: username,
+                                email:email,
+                                password:encrypted
+                            });
+                            newUser.save()
+                            res.json({"heading":"Success",
+                                            "statement":"Account created."});
+                        }
+                    }) 
                 }
             });
         }
@@ -39,12 +47,21 @@ router.post('/newuser', (req,res) => {
 router.post('/login', (req,res) => {
     console.log('What is poppin')
     const {username , password} = req.body
-    User.findOne({name:username, password:password}).then(user => {
+    User.findOne({name:username}).then(user => {
         if (user){
-            res.json({"heading":"Success",
-              "statement": "You have logged in successfully"});
+            bcrypt.compare(password,user.password,(err, same) => {
+                if (err) {
+                    res.json({"heading":"Error", "statement": "Could not log in. Please try again."});
+                } else {
+                    if (same) {
+                        res.json({"heading":"Success", "statement": "You have logged in successfully"});
+                    } else {
+                        res.json({"heading":"Incorrect password", "statement": "Please try again"});
+                    }
+                }
+            })
         } else {
-            res.json({"heading":"Incorrect username or password",
+            res.json({"heading":"Account with entered username does not exist",
               "statement": "Try again"});
         }
     });
