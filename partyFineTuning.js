706cc463dc1555d39a8604fde6ac77946a0bc943
Neloy <neloy@cpe-98-14-55-146.nyc.res.rr.com>
2021-01-03 22:15:16 -0500

    Tested getSongsWithLitness; it works

--- partyFineTuning.js
+++ partyFineTuning.js
@@ -1,5 +1,7 @@
 const axios = require('axios')
 
+
+
 //Returns promise to get song's ID
 function generateSongIDPromise(accessToken, song){
     const spotifyEndpointURL = 'https://api.spotify.com/v1/search'
@@ -36,7 +38,7 @@ function generateMultipleSongsAttributesPromise(accessToken, songIDList) {
             'Authorization' : `Bearer ${accessToken}`
         },
         params : {
-            "id" : idListString
+            "ids" : idListString
         }
     })
 }
@@ -55,7 +57,7 @@ async function getSongsWithLitness(accessToken,songs) {
         return response.data.tracks.items[0].id;
     })
     /*  
-    //SLOW METHOD
+    //SLOW METHOD ---!!!THIS METHOD HAS NOT BEEN TESTED YET!!!
     var getSongAttributesPromises = [];     
     for (var i = 0; i < songIds.length; i ++) {
         getSongAttributesResponses.push(generateSongAttributesPromise(accessToken,songIds[i]));   //Only using the single API requests at this point
@@ -66,15 +68,16 @@ async function getSongsWithLitness(accessToken,songs) {
     var getSongAttributesPromises = [];
     for (var i = 0; i < songIds.length; i += 100){
         var curSongIDList = [];
-        for (var j = 0; j < 100 && i + j < songIDs.length ; j ++) {
+        for (var j = 0; j < 100 && i + j < songIds.length ; j ++) {
             curSongIDList.push(songIds[i+j]);
         } 
         getSongAttributesPromises.push(generateMultipleSongsAttributesPromise(accessToken,curSongIDList));
     }
     const songMultipleAttributesResponses = await Promise.all(getSongAttributesPromises);
+
     const songIndividualAttributesResponses = [];
     for (var i = 0; i < songMultipleAttributesResponses.length ; i ++) {
-        const curResponseData = songMultipleAttributesResponses[i].data;
+        const curResponseData = songMultipleAttributesResponses[i].data.audio_features;
         for (var j = 0; j < curResponseData.length; j ++) {                 //Remember, 'curResponseData' is an array of each song's attributes
             songIndividualAttributesResponses.push(curResponseData[j]);
         }
@@ -90,23 +93,39 @@ async function getSongsWithLitness(accessToken,songs) {
         const energy_proportion = 2;
         const valence_proportion = 1; 
 
-        const litness_score = ((track_danceability*danceability_proportion)+(track_energy*energy_proportion)+(track_valence*valence_proportion))/3;
+        const litness_score = ((track_danceability*danceability_proportion)+(track_energy*energy_proportion)+(track_valence*valence_proportion))/(danceability_proportion+energy_proportion+valence_proportion);
 
         return litness_score;
     });
 
     var newSongs = songs; 
 
-    for (var i = 0; i < songs.length; i ++) {
-        newSongs.litness = songLitnesses[i];
+    for (var i = 0; i < newSongs.length; i ++) {
+        newSongs[i].litness = songLitnesses[i];
     }
-
+    console.log(newSongs);
     return newSongs;
 
 }
 
 
-
 module.exports = {
     getSongsWithLitness
 }
+
+//Everything that is under this line is to test the logic that has been presented above in this file
+//------------------------------------------------------------------------------------------------------------------------
+/*
+const theseSongs = [{
+    name : "Song 2",
+    artists: ["Blur"],
+    litness: 0
+},{
+    name : "Hyper Music",
+    artists : ["Muse"],
+    litness : 0
+}]
+
+dToken = "BQDUsFLlWu-ZAGOuzoFVfpAk3uG8LVcMsPvRRY3pAeMEhWOokJgKsiCUGIWgr8U7XN1PpuQhVBGN8WIpCF8_HGIFyMaXtU1LpkpXTwNhsB-ZplWrxI7JQEGiRIn9ZdkH7x_de-GqgEHrGcmie-5uQy3QaGzBTkXWO2yk1to8KJjxBQsTZT1sYBgxI46P23YCBpT_-DSphPxJeTzApi9BGprc4I29H9d0XLyado2N6772t6WxCAP31JaB3eZEkrY06wfLK2wmR41xrNA7"
+getSongsWithLitness(dToken,theseSongs)
+*/
